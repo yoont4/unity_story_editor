87be6363cd5783bf1ddbfc19038a9ecb6420376b
yoont4 <taylor.y1100@gmail.com>
2017-05-06 17:45:23 -0700

    removed unused variable

--- Assets/Editor/ClickManager.cs
+++ Assets/Editor/ClickManager.cs
@@ -13,9 +13,6 @@ public static class ClickManager {
 	// somewhere between 0.2 and 0.4 is good
 	public static float doubleClickFrame = 0.2f;
 	
-	// when this hits 2, 2 consecutive proper clicks have been made
-	private static int clickCount = 0;
-	
 	/*
 	  IsDoubleClick() takes the given time of a click, and determines 
 	  if it was a double click or not. 
