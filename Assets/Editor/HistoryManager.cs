9337deb76646125bc284d52968c9ea1b9ac74def
Taylor Yoon <taylor.y1100@gmail.com>
2017-05-15 15:25:22 -0700

    added temp TExtArea history recording with comments on how to properly implement it later.

--- Assets/Editor/HistoryManager.cs
+++ Assets/Editor/HistoryManager.cs
@@ -20,7 +20,16 @@ public static class HistoryManager {
 				// record all child containers of the node
 				tempContainer = node.childContainer;
 				while (tempContainer != null) {
-					Undo.RecordObject(tempContainer, "");
+					Undo.RegisterFullObjectHierarchyUndo(tempContainer, "");
+					
+					// TODO: this should be handled separately on all components that take input. It
+					// should not happen in a global "RecordEditor" function, it should be unique to input
+					// fields themselves. Probably attach as an event handler for on keypress or on change?
+					// something to batch multiple key presses for sure.
+					if (node.nodeType == NodeType.Dialog) {
+						Undo.RegisterCompleteObjectUndo(((DialogBox)tempContainer).dialogArea, "");
+					}
+					
 					Undo.RecordObject(tempContainer.outPoint, "");
 					tempContainer = tempContainer.child;
 				}
