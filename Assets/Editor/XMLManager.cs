558cf9ed657514cf41847eebec506500f419194d
yoont4 <taylor.y1100@gmail.com>
2017-07-12 01:43:06 -0700

    started work on XMLManager to export story node editor data into game engine useable format.

---
+++ Assets/Editor/XMLManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+
+/*
+  XMLManager is responsible for saving/loading XML data for game engine use.
+*/
+public static class XMLManager {
+	
+	public static StoryDialogEditor mainEditor;
+	
+	public static bool SaveXML(string path) {
+		// TODO: implement this
+		return false;
+	}
+}
+
+// ----------------------------------------------------------------------------------------------- //
+// ---------------------------------------- ENTRY CLASSES ---------------------------------------- //
+// ----------------------------------------------------------------------------------------------- //
+
