2ed9d081048d9c542adf364bef6ba3692f55431a
yoont4 <taylor.y1100@gmail.com>
2017-07-20 02:55:39 -0700

    made it so that StoryEditor base path and resource paths are handled automatically, so people don't need to edit the scripts at all when importing.

--- Assets/SDE/Editor/ProjectPathManager.cs
+++ Assets/SDE/Editor/ProjectPathManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
 
 /*
   ProjectPathManager just holds the currently used master path head of the project. That ways when saving
@@ -7,11 +9,82 @@
 */
 public static class ProjectPathManager {
 	
+	private static string _basePath = "";
+	
+	// returns the absolute path to the root of the StoryEditor project
 	public static string BasePath {
 		get {
-			return Application.dataPath + "/SDE";
+			if (string.IsNullOrEmpty(_basePath)) {
+				CalculateBasePath();
+			}
+			
+			return _basePath;
 		}
 	}
 	
-	public static string ResourcePath = "Assets/SDE/Editor/Resources";
+	private static string _resourcePath = "";
+	public static string ResourcePath {
+		get {
+			if (string.IsNullOrEmpty(_resourcePath)) {
+				CalculateResourcePath();
+			}
+			
+			return _resourcePath;
+		}
+	}
+	
+	static ProjectPathManager() {
+		if (string.IsNullOrEmpty(_basePath)) {
+			CalculateBasePath();
+		}
+		
+		if (string.IsNullOrEmpty(_resourcePath)) {
+			CalculateResourcePath();
+		}
+	}
+	
+	private static void CalculateBasePath() {
+		string projectPath;
+		
+		projectPath = Directory.GetFiles(Application.dataPath, "ProjectPathManager.cs", SearchOption.AllDirectories)[0];
+		
+		// normalize the filepath
+		projectPath = projectPath.Replace('\\', '/');
+		// remove the file and it's directory from the filepath
+		projectPath = projectPath.Replace("/Editor/ProjectPathManager.cs", "");
+		
+		_basePath = projectPath;
+		
+		Debug.Log("calculated StoryEditor project base path: " + _basePath);
+	}
+	
+	private static void CalculateResourcePath() {
+		// make sure that the base path has been calculated already
+		if (string.IsNullOrEmpty(_basePath)) {
+			CalculateBasePath();
+		}
+		
+		// split the path on each directory
+		string[] splitPath = _basePath.Split('/');
+		
+		// locate the "Asset" string and build from there
+		int assetIndex = -1;
+		for (int i = 0; i < splitPath.Length; i++) {
+			if (splitPath[i] == "Assets") {
+				assetIndex = i;
+			}
+		}
+		
+		if (assetIndex < 0) {
+			Debug.Log("PROJECT PATH ERROR: couldn't calculate resource path from base!");
+		} else {
+			_resourcePath = "";
+			for (int i = assetIndex; i < splitPath.Length; i++) {
+				_resourcePath += splitPath[i] + '/';
+			}
+			_resourcePath += "Editor/Resources";
+		}
+		
+		Debug.Log("calculated StoryEditor resource path: " + _resourcePath);
+	}
 }
