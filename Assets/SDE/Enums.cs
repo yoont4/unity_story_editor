2533600b50c92ab32ac3f09867e98e33511e9cad
yoont4 <taylor.y1100@gmail.com>
2017-07-20 04:20:10 -0700

    reformatted Enums.cs style because it was poopy.

--- Assets/SDE/Enums.cs
+++ Assets/SDE/Enums.cs
@@ -2,10 +2,34 @@
   All enums used across the Editor and it's serialized entry formats are stored here.
 */
 
-public enum NodeType {Nothing, Dialog, Decision, SetLocalFlag, SetGlobalFlag, CheckLocalFlag, CheckGlobalFlag, Interrupt}
+public enum NodeType {
+	Nothing,
+	Dialog,
+	Decision,
+	SetLocalFlag,
+	SetGlobalFlag,
+	SetGlobalVariable,
+	CheckLocalFlag,
+	CheckGlobalFlag,
+	CheckGlobalVariable,
+	Interrupt
+}
 
-public enum SDEComponentType {Nothing, Node, ConnectionPoint, TextArea, Label}
+public enum SDEComponentType {
+	Nothing,
+	Node,
+	ConnectionPoint,
+	TextArea,
+	Label
+}
 
-public enum SDEContainerType {DialogBox, DialogInterrupt, DecisionBox}
+public enum SDEContainerType {
+	DialogBox,
+	DialogInterrupt,
+	DecisionBox
+}
 
-public enum ConnectionPointType { In, Out }
+public enum ConnectionPointType {
+	In,
+	Out
+}
